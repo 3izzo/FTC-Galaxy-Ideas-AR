a588c4c562105eef453f3bc91f2b993b7ee28358
3izzo <obharabi@gmail.com>
2019-11-30 11:46:33 +0300

    Init

---
+++ Assets/EnableAnimationOnFound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAnimationOnFound : MonoBehaviour
+{
+    private bool rendering = false;
+    private Renderer renderer;
+    public Animation anim;
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (renderer.enabled != rendering)
+        {
+            rendering = renderer.enabled;
+            if (rendering)
+            {
+                // anim.clip.wrapMode = WrapMode.Loop;
+                anim.Play();
+            }
+            else
+                anim.Stop();
+        }
+    }
+}
